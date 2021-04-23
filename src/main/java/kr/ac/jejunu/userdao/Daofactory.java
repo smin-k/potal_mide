7d9ad39f31441e36fc26eb64260504438a0e0b98
tmdals8030 <48111893+tmdals8030@users.noreply.github.com>
2021-04-23 17:52:40 +0900

    문제11

--- src/main/java/kr/ac/jejunu/userdao/Daofactory.java
+++ src/main/java/kr/ac/jejunu/userdao/Daofactory.java
@@ -3,6 +3,7 @@ package kr.ac.jejunu.userdao;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
